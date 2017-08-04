0fe67afe345a18a2d2fee175c2ecf6c25e742f54
vitorDaynno <vitordaynno@gmail.com>
2017-08-03 23:30:02 -0300

    Algoritmo funcionando

---
+++ cifraCesar.py
@@ -0,0 +1,22 @@
+alfa = [' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','w','z']
+
+texto = 'Jogue me aos lobos e voltarei como lider deles' #insira o texto aqui
+texto = texto.lower()
+resposta = ''
+
+i = 0
+while i < len(texto):
+	if texto[i] == ' ':
+		resposta = resposta + ' '
+	elif texto[i] == 'y':
+		resposta = resposta + 'a'
+	elif texto[i] == 'w':
+		resposta = resposta +'b'
+	elif texto[i] == 'z':
+		resposta = resposta +'c'
+	else:
+		indice = alfa.index(texto[i])
+		resposta = resposta + alfa[indice + 3]
+	i = i + 1
+
+print resposta
